555a9605de990ee4c4f8ae7e8da1d33268bc4d1b
myconviction <dntkdgur1216@khu.ac.kr>
2025-06-08 01:44:45 +0900

    C:/Program Files/Git/etc/systemd/system/download-preprocess.service의 자동실행을 위해

--- app/config.py
+++ app/config.py
@@ -1,15 +1,17 @@
 import os
-from pydantic import BaseSettings
+#from pydantic import BaseSettings
+#class Settings(BaseSettings):
 
-class Settings(BaseSettings):
+#현재 env는 무시하고 config.py에서 설정된값만 사용합니다.
+class Settings:
     PORT: int = 8000
     DEBUG: bool = False
     TMP_ROOT: str = "/tmp/download_preprocess"
     WHISPER_URL: str = "http://54.180.24.69:8002/internal/transcribe"
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+#   class Config:
+#       env_file = ".env"
+#       env_file_encoding = "utf-8"
 
 def load_settings() -> Settings:
     return Settings()
